bb1684f79544b28c7ad7163abf0a5d332c356eb4
meowljane <69661367+meowljane@users.noreply.github.com>
2021-08-01 02:54:04 +0900

    Add files via upload

---
+++ A16/script.js
@@ -0,0 +1,55 @@
+$('#content')
+  .on("dragover", dragOver)
+  .on("dragleave", dragOver)
+  .on("drop", uploadFiles);
+
+function dragOver(e){
+	e.stopPropagation();
+	e.preventDefault();
+	if (e.type == "dragover") {
+		$(e.target).css({
+			"background-color": "black",
+			"outline-offset": "-20px"
+		});
+	} else {
+    	$(e.target).css({
+			"background-color": "black",
+			"outline-offset": "-10px"
+		});
+    }
+}
+
+function uploadFiles(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    dragOver(e);
+  
+    e.dataTransfer = e.originalEvent.dataTransfer;
+    var files = e.target.files || e.dataTransfer.files;
+    if (files.length > 1) {
+        alert('하나만 올려주세요.');
+        return;
+    }
+
+if (files[0].name.match(/crossword.png/)){
+$('#crossword').css({"background-image": "url(" + window.URL.createObjectURL(files[0]) + ")",
+            "outline": "none",
+            "background-size": "100% 100%",
+"z-index":"5",
+"width":"800px",
+"height":"638px"})
+.draggable();
+$('#문제').css({"pointer-events":"none","user-select":"none","z-index":"+=10"});
+$('#문제2').css({"pointer-events":"none","user-select":"none","z-index":"+=5"});
+$('#content').css({"pointer-events":"none","user-select":"none","z-index":"-=15"});
+}
+
+else{
+      alert('올바른 아이템을 올려주세요.');
+      return;
+    }
+}
+$( '#crossword').css({"z-index":"5"});
+$( '#문제').css({"z-index":"10"});
+$( '#문제2').css({"z-index":"8"});
+$( '#content').css({"z-index":"15"});
