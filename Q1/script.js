8c72546b81e92724fcd0780c8fb660429385d3b4
meowljane <69661367+meowljane@users.noreply.github.com>
2021-08-03 03:21:31 +0900

    Add files via upload

--- Q1/script.js
+++ Q1/script.js
@@ -40,22 +40,45 @@ function uploadFiles(e) {
 
 
 if (files[0].name.startsWith('Item_톱니바퀴')){
-      alert('좋은 생각입니다만, 굳이 지금 할 필요가 있을까요?');
+         action_popup.alert("좋은 생각이긴 한데...아직은 필요 없어 보여요");
       return;
 }
 
 if (files[0].name.startsWith('Item_edit')){
-      alert('그 아이템은 이제 필요 없어요. 버려주세요');
+      action_popup.alert('그 아이템은 이제 필요 없어요. 버려주세요');
       return;
 }
 
 if (files[0].name.startsWith('Item_CD')){
-      alert('지금 노래가 맘에 안드신다는거죠? 어디 이 노래는 어때요?\n문제 풀이랑은 상관 없어요. 분위기나 바꿔보려는거예요.');
+      action_popup.alert('지금 노래가 맘에 안드신다는거죠? 어디 이 노래는 어때요?\n문제 풀이랑은 상관 없어요. 분위기나 바꿔보려는거예요.');
       return;
 }
 
 else{
-      alert('올바른 아이템을 올려주세요.');
+      action_popup.alert('올바른 아이템을 올려주세요.');
       return;
     }
 }
+
+$(".modal_close").on("click", function () {
+      action_popup.close(this);
+  });
+var action_popup = {
+      timer: 500,
+    alert: function (txt) {this.open("type-alert", txt);},
+  
+      open: function (type, txt) {
+          var popup = $("." + type);
+          popup.find(".menu_msg").text(txt);
+          $("body").append("<div class='dimLayer'></div>");
+          $(".dimLayer").css('height', $(document).height()).attr("target", type);
+          popup.fadeIn(this.timer);
+      },
+  
+      close: function (target) {
+          var modal = $(target).closest(".modal-section");
+          var dimLayer = $(".dimLayer[target=type-alert]")
+          modal.fadeOut(this.timer);
+          setTimeout(function () {dimLayer != null ? dimLayer.remove() : "";}, this.timer);
+      }
+  }
