f694c381ba59fc3a27f1d8fe297b040193f1af23
meowljane <69661367+meowljane@users.noreply.github.com>
2021-08-11 16:17:15 +0900

    Update script.js

--- Q15/script.js
+++ Q15/script.js
@@ -32,10 +32,10 @@ function uploadFiles(e) {
     e.dataTransfer = e.originalEvent.dataTransfer;
     var files = e.target.files || e.dataTransfer.files;
     if (files.length > 1) {
-        alert('하나만 올려주세요.');
+        action_popup.alert('하나만 올려주세요.');
         return;
     }
-    if (files[0].name.startsWith('렌즈')){
+    if (files[0].name.startsWith('Item_렌즈')){
       $('#공1').fadeIn(2000);
       var audio = new Audio("./done.mp3");
       audio.play();
@@ -48,7 +48,7 @@ function uploadFiles(e) {
       return;
       }
 
-    if (files[0].name.startsWith('동전')){
+    if (files[0].name.startsWith('Item_동전')){
       $('#공2').fadeIn(2000);
       var audio = new Audio("./done.mp3");
       audio.play();
@@ -61,7 +61,7 @@ function uploadFiles(e) {
       return;
       }
    
-    if (files[0].name.startsWith('반지')){
+    if (files[0].name.startsWith('Item_반지')){
       $('#공3').fadeIn(2000);
       var audio = new Audio("./done.mp3");
       audio.play();
@@ -74,7 +74,7 @@ function uploadFiles(e) {
       return;
       }
 
-    if (files[0].name.startsWith('구슬')){
+    if (files[0].name.startsWith('Item_구슬')){
       $('#공4').fadeIn(2000);
       var audio = new Audio("./done.mp3");
       audio.play();
@@ -88,23 +88,46 @@ function uploadFiles(e) {
       }
 
 
-if (files[0].name.startsWith('튜토리얼')){
-      alert('그 아이템은 이제 필요 없어요. 버려주세요');
+if (files[0].name.startsWith('Item_edit')){
+      aaction_popup.alert('그 아이템은 이제 필요 없어요. 버려주세요');
       return;
 }
 
-if (files[0].name.startsWith('CD')){
-      alert('노래 말고 문제에 집중해주세요.\n틀 노래가 떨어져서 이러는건 아니에요.');
+if (files[0].name.startsWith('Item_CD')){
+     action_popup.alert('노래 말고 문제에 집중해주세요.\n틀 노래가 떨어져서 이러는건 아니에요.');
       return;
 }
 
-if (files[0].name.startsWith('상자')){
-      alert('그게 아니에요. 여기 너무 어둡지 않아요?');
+if (files[0].name.startsWith('Item_상자')){
+      action_popup.alert('그게 아니에요. 여기 너무 어둡지 않아요?');
       return;
 }
 
 else{
-      alert('올바른 아이템을 올려주세요.');
+      action_popup.alert('올바른 아이템을 올려주세요.');
       return;
     }
     }
+
+$(".modal_close").on("click", function () {
+      action_popup.close(this);
+  });
+var action_popup = {
+      timer: 500,
+    alert: function (txt) {this.open("type-alert", txt);},
+  
+      open: function (type, txt) {
+          var popup = $("." + type);
+          popup.find(".menu_msg").text(txt);
+          $("body").append("<div class='dimLayer'></div>");
+          $(".dimLayer").css('height', $(document).height()).attr("target", type);
+          popup.fadeIn(this.timer);
+      },
+  
+      close: function (target) {
+          var modal = $(target).closest(".modal-section");
+          var dimLayer = $(".dimLayer[target=type-alert]")
+          modal.fadeOut(this.timer);
+          setTimeout(function () {dimLayer != null ? dimLayer.remove() : "";}, this.timer);
+      }
+  }
