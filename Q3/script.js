b0332e4785146858e2abb7f2377be40937497395
meowljane <69661367+meowljane@users.noreply.github.com>
2021-08-11 14:59:39 +0900

    Update script.js

--- Q3/script.js
+++ Q3/script.js
@@ -27,33 +27,55 @@ function uploadFiles(e) {
     e.dataTransfer = e.originalEvent.dataTransfer;
     var files = e.target.files || e.dataTransfer.files;
     if (files.length > 1) {
-        alert('하나만 올려주세요.');
+        action_popup.alert('하나만 올려주세요.');
         return;
     }
 
     if (files[0].name.startsWith('Item_전구')){
-	alert('전 문제 덕분에 이곳은 점점 밝아지고 있어요');
-      audio.play();
+	action_popup.alert('전 문제 덕분에 이곳은 점점 밝아지고 있어요');
       return;
       }
 
 if (files[0].name.startsWith('Item_edit')){
-      alert('그 아이템은 이제 필요 없어요. 버려주세요');
+      action_popup.alert('그 아이템은 이제 필요 없어요. 버려주세요');
       return;
 }
 
 if (files[0].name.startsWith('Item_CD')){
-      alert('노래 말고 문제에 집중해주세요.\n틀 노래가 떨어져서 이러는건 아니에요.');
+      action_popup.alert('노래 말고 문제에 집중해주세요.\n틀 노래가 떨어져서 이러는건 아니에요.');
       return;
 }
 
 if (files[0].name.startsWith('Item_상자')){
-      alert('이 문제는 아이템 없이 풀 수 있을지도 몰라요');
+      action_popup.alert('이 문제는 아이템 없이 풀 수 있을지도 몰라요');
       return;
 }
 
 else{
-      alert('올바른 아이템을 올려주세요.');
+      action_popup.alert('올바른 아이템을 올려주세요.');
       return;
     }
 }
+
+$(".modal_close").on("click", function () {
+      action_popup.close(this);
+  });
+var action_popup = {
+      timer: 500,
+    alert: function (txt) {this.open("type-alert", txt);},
+  
+      open: function (type, txt) {
+          var popup = $("." + type);
+          popup.find(".menu_msg").text(txt);
+          $("body").append("<div class='dimLayer'></div>");
+          $(".dimLayer").css('height', $(document).height()).attr("target", type);
+          popup.fadeIn(this.timer);
+      },
+  
+      close: function (target) {
+          var modal = $(target).closest(".modal-section");
+          var dimLayer = $(".dimLayer[target=type-alert]")
+          modal.fadeOut(this.timer);
+          setTimeout(function () {dimLayer != null ? dimLayer.remove() : "";}, this.timer);
+      }
+  }
