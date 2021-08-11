e0a1a09a58de5e079d9d21bb201fbf302f89a02d
meowljane <69661367+meowljane@users.noreply.github.com>
2021-08-11 15:53:04 +0900

    Update script.js

--- Q14/script.js
+++ Q14/script.js
@@ -27,34 +27,52 @@ function uploadFiles(e) {
     e.dataTransfer = e.originalEvent.dataTransfer;
     var files = e.target.files || e.dataTransfer.files;
     if (files.length > 1) {
-        alert('하나만 올려주세요.');
+ action_popup.alert('하나만 올려주세요.');
         return;
     }
 
-    if (files[0].name.startsWith('고무호스')){
-      $('#Q2').fadeIn(2000);
-      var audio = new Audio("./done.mp3");
-      audio.play();
-      return;
-      }
 
-if (files[0].name.startsWith('튜토리얼')){
-      alert('그 아이템은 이제 필요 없어요. 버려주세요');
+
+if (files[0].name.startsWith('Item_edit')){
+      action_popup.alert('그 아이템은 이제 필요 없어요. 버려주세요');
       return;
 }
 
-if (files[0].name.startsWith('CD')){
-      alert('노래 말고 문제에 집중해주세요.\n틀 노래가 떨어져서 이러는건 아니에요.');
+if (files[0].name.startsWith('Item_CD')){
+       action_popup.alert('노래 말고 문제에 집중해주세요.\n틀 노래가 떨어져서 이러는건 아니에요.');
       return;
 }
 
-if (files[0].name.startsWith('상자')){
-      alert('그게 아니에요. 여기 너무 어둡지 않아요?');
+if (files[0].name.startsWith('Item_상자')){
+       action_popup.alert(그게 아니에요. 여기 너무 어둡지 않아요?');
       return;
 }
 
 else{
-      alert('올바른 아이템을 올려주세요.');
+       action_popup.alert('올바른 아이템을 올려주세요.');
       return;
     }
 }
+
+$(".modal_close").on("click", function () {
+      action_popup.close(this);
+  });
+var action_popup = {
+      timer: 500,
+    alert: function (txt) {this.open("type-alert", txt);},
+  
+      open: function (type, txt) {
+          var popup = $("." + type);
+          popup.find(".menu_msg").text(txt);
+          $("body").append("<div class='dimLayer'></div>");
+          $(".dimLayer").css('height', $(document).height()).attr("target", type);
+          popup.fadeIn(this.timer);
+      },
+  
+      close: function (target) {
+          var modal = $(target).closest(".modal-section");
+          var dimLayer = $(".dimLayer[target=type-alert]")
+          modal.fadeOut(this.timer);
+          setTimeout(function () {dimLayer != null ? dimLayer.remove() : "";}, this.timer);
+      }
+  }
