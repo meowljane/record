36cd82304d18b8030ac33cd89c6d75a1b1f20a00
meowljane <69661367+meowljane@users.noreply.github.com>
2021-08-11 16:19:44 +0900

    Update script.js

--- Q16/script.js
+++ Q16/script.js
@@ -27,11 +27,11 @@ function uploadFiles(e) {
     e.dataTransfer = e.originalEvent.dataTransfer;
     var files = e.target.files || e.dataTransfer.files;
     if (files.length > 1) {
-        alert('하나만 올려주세요.');
+        action_popup.alert('하나만 올려주세요.');
         return;
     }
 
-if (files[0].name.match(/crossword.png/)){
+if (files[0].name.startsWith('Item_크로스워드')){
 $('#crossword').css({"background-image": "url(" + window.URL.createObjectURL(files[0]) + ")",
             "outline": "none",
             "background-size": "100% 100%",
@@ -44,10 +44,34 @@ $('#content').css({"pointer-events":"none","user-select":"none","z-index":"-=15"
 }
 
 else{
-      alert('올바른 아이템을 올려주세요.');
+      action_popup.alert('올바른 아이템을 올려주세요.');
       return;
     }
 }
 $( '#crossword').css({"z-index":"5"});
 $( '#문제').css({"z-index":"10"});
 $( '#content').css({"z-index":"15"});
+
+
+$(".modal_close").on("click", function () {
+      action_popup.close(this);
+  });
+var action_popup = {
+      timer: 500,
+    alert: function (txt) {this.open("type-alert", txt);},
+  
+      open: function (type, txt) {
+          var popup = $("." + type);
+          popup.find(".menu_msg").text(txt);
+          $("body").append("<div class='dimLayer'></div>");
+          $(".dimLayer").css('height', $(document).height()).attr("target", type);
+          popup.fadeIn(this.timer);
+      },
+  
+      close: function (target) {
+          var modal = $(target).closest(".modal-section");
+          var dimLayer = $(".dimLayer[target=type-alert]")
+          modal.fadeOut(this.timer);
+          setTimeout(function () {dimLayer != null ? dimLayer.remove() : "";}, this.timer);
+      }
+  }
