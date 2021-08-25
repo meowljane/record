ad6adecbad42ccdbf497b22f10ed7296b825c3d7
meowljane <69661367+meowljane@users.noreply.github.com>
2021-08-25 18:48:41 +0900

    Update script2.js

--- Q5/script2.js
+++ Q5/script2.js
@@ -31,7 +31,7 @@ function uploadFiles(e) {
         return;
     }
 
-    if (files[0].name.startsWith('Item_돋보기')){
+    if (files[0].name.startsWith('Item_확대마법')){
       $('#mag').css({'visibility': 'visible', 'opacity' : '1'});
       $('#Q2').css({'opacity' : '1'});
       var audio = new Audio("./done.mp3");
@@ -44,12 +44,12 @@ if (files[0].name.startsWith('Item_edit')){
       return;
 }
 
-if (files[0].name.startsWith('Item_CD')){
+if (files[0].name.startsWith('Item_음악마법')){
       alert('노래 말고 문제에 집중해주세요.\n틀 노래가 떨어져서 이러는건 아니에요.');
       return;
 }
 
-if (files[0].name.startsWith('Item_상자')){
+if (files[0].name.startsWith('Item_소환마법')){
       alert('그게 아니에요. 이친구 조사를 해야할 것 같지 않아요?');
       return;
 }
