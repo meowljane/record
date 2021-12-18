00444ff579471f76e63beb7a67d25b014d754c17
meowljane <69661367+meowljane@users.noreply.github.com>
2021-12-19 00:59:44 +0900

    Update script.js

--- Q1/script.js
+++ Q1/script.js
@@ -65,11 +65,8 @@ if (files[0].name.startsWith('Item_음악마법')){
 	else if(track==2){action_popup.alert('마법으로 하는거라고? \n 하나 더 들려줘!');
 		     $("#track2")[0].pause(); $("#track3")[0].play();
 		    track=3}
-	else if(track==3){action_popup.alert('너무 신기하다!! \n 음악 자주 들려줘!!');
-		     $("#track3")[0].pause(); $("#track4")[0].play();
-		    track=4}
 	else {action_popup.alert('그거 들려줘! 처음 들려준거!!');
-		     $("#track4")[0].pause(); $("#track1")[0].play();
+		     $("#track3")[0].pause(); $("#track1")[0].play();
 		    track=1}
       return;
 }
